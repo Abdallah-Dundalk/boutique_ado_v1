d9fc18a6900b4311f24be0744433243d65bd2a25
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-12-29 17:24:12 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MK3JuCPvGNCIKNA4xArHVWlFFo4SLhBblQYe7BBWOboH0vUEMuf0DXmBXNQHfJ9hIrkzaVnhqXQolZ0HmguleP900HSbTDuhQ',
+        'client_secret': 'test client secret',
     }
 
-    return render(request, template, context )
+    return render(request, template, context)
