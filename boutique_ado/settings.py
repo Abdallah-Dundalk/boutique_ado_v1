19370606e82de96785c265f2766328e390f77418
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2023-01-16 20:59:33 +0000

    Fix type in bucket name.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,11 +177,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_name = 'boutique-ado-1'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-1'
     AWS_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_name}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
